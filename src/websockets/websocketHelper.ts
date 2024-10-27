e6af6a5ef227499837b966ca058e24dad7707b4a
javirrof <javirofe96@gmail.com>
2024-10-27 08:27:39 +0100

    update websocket server to send messages to corresponding chat

--- src/websockets/websocketHelper.ts
+++ src/websockets/websocketHelper.ts
@@ -1,22 +1,38 @@
-import { WebSocket } from "ws"
-import { WebSocketsMessage } from "./websockets"
+import { WebSocket } from 'ws'
+import { ChatType, WebSocketsMessage } from './websockets'
 
 export const generateSocketId = () => {
   return Math.random().toString(36).substring(2, 15)
 }
-export const welcomeMessage = (socketId: string): string => {
-  const message = { type: 'welcome', sender: 'server', content: 'Welcome to the server', receiver: socketId }
+export const welcomeMessage = (socketId: string, chat: ChatType): string => {
+  const message: WebSocketsMessage = {type: 'join', socketId, content: 'Welcome to the chat', chat}
   const stringMessage = JSON.stringify(message)
   return stringMessage
 }
 
-export const sendToClient = (websocketMap: Map<string, WebSocket>, socketId: string, message: WebSocketsMessage ) => {
+export const sendMessageToChat = (websocketMap: Map<string, WebSocket>, socketIdChatMap: Map<ChatType, string[]>, receivedMessage: string) => {
+  const message = JSON.parse(receivedMessage) as WebSocketsMessage
+  const ids: string[] = socketIdChatMap.get(message.chat) as string[]
+  if (!ids) return
+  ids.forEach((id) => {
+    const ws = websocketMap.get(id)
+    if (!ws) return
+    if (ws.readyState !== ws.OPEN) return
+    if (id === message.socketId) return
+    ws.send(JSON.stringify(message))
+  })
+}
+
+export const sendToClient = (websocketMap: Map<string, WebSocket>, socketId: string, message: WebSocketsMessage) => {
   const ws = websocketMap.get(socketId)
   if (!ws) return
+  if (ws.readyState !== ws.OPEN) return
   ws.send(JSON.stringify(message))
 }
 
 export const sendToAllClients = (websocketMap: Map<string, WebSocket>, message: WebSocketsMessage) => {
-  websocketMap.forEach((ws) => ws.send(JSON.stringify(message)))
+  websocketMap.forEach((ws) => {
+    if (ws.readyState !== ws.OPEN) return
+    ws.send(JSON.stringify(message))
+  })
 }
-
--- src/websockets/websockets.ts
+++ src/websockets/websockets.ts
@@ -1,13 +1,16 @@
 import { Server } from 'http'
 import { WebSocket, WebSocketServer } from 'ws'
-import { generateSocketId, sendToAllClients, sendToClient, welcomeMessage } from './websocketHelper'
+import { generateSocketId, sendMessageToChat, sendToAllClients, sendToClient, welcomeMessage } from './websocketHelper'
+import url from 'url'
 
-export type WebSocketMessageType = 'welcome' | 'group' | 'individual' | 'close'
+export type WebSocketMessageType = 'join' | 'message' | 'leave'
+
+export type ChatType = 'pets' | 'football' | 'movies'
 export interface WebSocketsMessage {
   type: WebSocketMessageType
-  sender: string
-  receiver: string | 'group'
   content: string
+  chat: ChatType
+  socketId: string
 }
 export interface ExtendedWebSocket extends WebSocket {
   isAlive: boolean
@@ -15,19 +18,29 @@ export interface ExtendedWebSocket extends WebSocket {
 
 export const websocketMap = new Map<string, ExtendedWebSocket>()
 export const reverseWebsocketMap = new Map<ExtendedWebSocket, string>()
+export const socketIdChatMap = new Map<ChatType, string[]>()
 
 export const createWebsocketServer = (server: Server) => {
   const wss = new WebSocketServer({ server })
 
-  wss.on('connection', (ws: WebSocket) => {
+  wss.on('connection', (ws: ExtendedWebSocket, req: Request) => {
+    const query = url.parse(req.url as string, true).query
+    const chat = query.chat as ChatType
     const extendedWs = ws as ExtendedWebSocket
     const socketId = generateSocketId()
     websocketMap.set(socketId, extendedWs)
     reverseWebsocketMap.set(extendedWs, socketId)
+    if (!socketIdChatMap.has(chat)) {
+      socketIdChatMap.set(chat, [socketId])
+    } else {
+      const chatArray = socketIdChatMap.get(chat) as string[]
+      chatArray?.push(socketId)
+      socketIdChatMap.set(chat, chatArray)
+    }
     console.log(`Client connected with id: ${socketId}. Number of clients: `, websocketMap.size)
 
     // Send welcome message to client
-    ws.send(welcomeMessage(socketId))
+    ws.send(welcomeMessage(socketId, chat))
 
     // Set interval to check is socket is alive
     const aliveInterval = setInterval(() => {
@@ -46,17 +59,14 @@ export const createWebsocketServer = (server: Server) => {
 
     // Listen for messages in this client
     ws.on('message', (message) => {
-      const parsedMessage: WebSocketsMessage = JSON.parse(message.toString())
-      const { sender, receiver, type } = parsedMessage
-      const ws = websocketMap.get(sender)
+      const parsedMessage = JSON.parse(message.toString()) as WebSocketsMessage
+      if (parsedMessage.type === 'message') {
+        sendMessageToChat(websocketMap, socketIdChatMap, message.toString())
+      }
+
       if (!ws) return
       ws.isAlive = true
 
-      if (type === 'individual' && receiver !== 'group') {
-        sendToClient(websocketMap, receiver, parsedMessage)
-      } else if (type === 'group') {
-        sendToAllClients(websocketMap, parsedMessage)
-      }
       console.log('Message:', parsedMessage)
     })
 
